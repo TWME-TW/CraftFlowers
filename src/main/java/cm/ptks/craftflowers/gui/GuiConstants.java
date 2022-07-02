9317a5c6de54b454fe5aacab82c6f4072c030824
TWME <65117253+TWME-TW@users.noreply.github.com>
2022-07-02 16:07:48 +0800

    Update GuiConstants.java

--- src/main/java/cm/ptks/craftflowers/gui/GuiConstants.java
+++ src/main/java/cm/ptks/craftflowers/gui/GuiConstants.java
@@ -11,11 +11,11 @@ public class GuiConstants {
             .setDisplayName("§a").build();
 
     public static final ItemStack PREV = new ItemBuilder(Material.PLAYER_HEAD)
-            .setDisplayName(CraftFlowers.arrow + "§2Previous")
+            .setDisplayName(CraftFlowers.arrow + "§2上一頁")
             .setTextureId("8652e2b936ca8026bd28651d7c9f2819d2e923697734d18dfdb13550f8fdad5f").build();
 
     public static final ItemStack NEXT = new ItemBuilder(Material.PLAYER_HEAD)
-            .setDisplayName(CraftFlowers.arrow + "§2Next")
+            .setDisplayName(CraftFlowers.arrow + "§2下一頁")
             .setTextureId("2a3b8f681daad8bf436cae8da3fe8131f62a162ab81af639c3e0644aa6abac2f").build();
 
     private GuiConstants() {
